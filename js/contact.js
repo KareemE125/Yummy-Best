96f00bf1e348d79109528534cf6450a94b2d9ca7
KareemE125 <kareemezzat.125@gmail.com>
2022-10-28 20:08:18 +0200

    init contact

---
+++ js/contact.js
@@ -0,0 +1,8 @@
+const inputs = $('#contact input');
+
+const nameInput = $('#contact input').eq(0);
+const emailInput = $('#contact input').eq(1);
+const phoneInput = $('#contact input').eq(2);
+const ageInput = $('#contact input').eq(3);
+const passwordInput = $('#contact input').eq(4);
+const repasswordInput = $('#contact input').eq(5);
